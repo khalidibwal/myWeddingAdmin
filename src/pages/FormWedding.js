58898f5dc6322acefaa00454c287958fa46e30ea
khalidibwal <khalidibwal@gmail.com>
2022-08-31 20:20:33 +0700

    login auth success

--- src/pages/FormWedding.js
+++ src/pages/FormWedding.js
@@ -1,26 +1,29 @@
 import { Card, TextField, Grid, Stack, Button } from '@mui/material';
 import { Container } from '@mui/system';
 import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom';
-import React,{useEffect} from 'react'
+import { useNavigate, useLocation } from 'react-router-dom';
+import React,{useEffect, useState} from 'react'
 import Page from '../components/Page';
 import Label from '../components/Label';
 
 
 function FormWedding() { 
   const Navigate = useNavigate();
-  const {auth} = useParams();
+  const Location = useLocation();
+  const [myUsername, setMyusername] = useState('')
   useEffect(() => {
     const isAuthorized = () =>{
       axios.get(`https://x8ki-letl-twmt.n7.xano.io/api:_G_SfNPu/auth/me`,{
         headers:{
-          'Authorization' : auth
+          'Authorization' : Location.state
         }
       })
       .then((response)=> {
-        console.log(response, 'success')
+        if(response.state === 200){
+          setMyusername(response.data.name)
+        }
       })
-      .catch((e)=> Navigate('/login'))
+      .catch((e)=> console.log(e))
     }
     isAuthorized();    
   }, []);
@@ -36,7 +39,7 @@ function FormWedding() {
             <TextField id="outlined-basic" label="Name" variant="outlined" name='name' />
             <TextField id="outlined-basic" label="Outlined" variant="outlined" />
             <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            <Button variant="contained">Kirim</Button>
+            <Button variant="contained">{myUsername}</Button>
             </Stack>
           </Container>
         </Card>
--- src/sections/auth/login/LoginForm.js
+++ src/sections/auth/login/LoginForm.js
@@ -57,8 +57,9 @@ export default function LoginForm() {
     .then((response)=> {
       if(response.status === 200){
         navigate('/dashboard/wedding', {
-          auth: response.data.authToken
+          state: response.data.authToken
         })
+        console.log(response.data.authToken)
     }})
     .catch((error)=> swal("Warning!", "Data yang anda masukan salah!", "error"))
   }
